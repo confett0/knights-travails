c74b34f0addd9b633bf0e901436a035d346a0ebd
confett0 <118047961+confett0@users.noreply.github.com>
2023-09-26 18:16:14 +0200

    Remove getNextMoves function

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 class Knight {
-    constructor(x,y) {
-        this.position = [x,y];
+    constructor(xPos,yPos) {
+        this.position = [xPos,yPos];
+        this.path = [[xPos,yPos]];
     }
+}
 
-    getNextMoves() {
-        const possibleMoves = [[+1,+2],[+1,-2],[-1,+2],[-1,-2],[+2,+1],[+2,-1],[-2,+1],[-2,-1]];
-        return possibleMoves
-        .map(([xCoord, yCoord]) => [xCoord + this.position[0], yCoord + this.position[1]])
-        .filter(([xCoord, yCoord]) => (xCoord >= 0 && xCoord < 8) && (yCoord >= 0 && yCoord < 8));
+const knightMoves = ([x,y],[a,b]) => {
+    if ((x < 0 || x >= 8) || (y < 0 || y >= 8) || (a < 0 || b >= 8) (a < 0 || b >= 8)) {
+        return "Coordinates out of bound";
     }
+    if (x === a && y === b) return "You're already where you want to get!";
+
+    // Create a chessboard as a 2D array to keep track of visited cells and paths
+    const chessboard = Array(8).fill().map(() => Array(8).fill({ visited: false, path: [] }));
+
+    // Define knight's possible moves
+    const possibleMoves = [[+1,+2],[+1,-2],[-1,+2],[-1,-2],[+2,+1],[+2,-1],[-2,+1],[-2,-1]];
+
+
+    const knigth = new Knight(x,y);
+    const moves = getNextMoves([x,y]);
+    const queue = [];
+
+    for (const )
+
 }
